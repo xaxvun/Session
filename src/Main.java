71e9f8699c375ef82f8357abce4e655c868c623c
kssosoy <qws1566@naver.com>
2024-03-19 18:31:33 +0900

    1주차 세션 과제

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Test");
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for(int i=0; i<100; i++){
+            list.add(i+1);
+            System.out.println("Test");
+        }
     }
 }
